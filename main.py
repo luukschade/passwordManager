6f354825b2e2bc0b633ba48a4896f5b883bd3629
Luuk Schade <luuk@Luuks-MacBook-Air.local>
2026-08-18 12:36:48 +0200

    v1.0

---
+++ main.py
@@ -0,0 +1,73 @@
+accounts = {
+}
+
+id = 1
+
+def add_password():
+    service = str(input("Which service: "))
+    username = str(input("What is your username: "))
+    password = str(input("What is your password: "))
+    accounts[id] = {"Service": service, "Username": username, "Password": password}
+
+def delete_password():
+    del_password_id = int(input("The ID of the password you want to delete: "))
+    try:
+        del accounts[del_password_id]
+    except KeyError:
+        print("That is not a valid ID!")
+
+def show_passwords():
+    for account_id, info in accounts.items():
+        print(f"Account ID: {account_id}")
+        print(f"Service: {info['Service']}")
+        print(f"Username: {info['Username']}")
+        print(f"Password: {info['Password']}")
+
+def edit_password():
+    while True:
+        edit_password_id = int(input("The ID of the password you want to edit: "))
+        try:
+            while True:
+                print("1. Service")
+                print("2. Username")
+                print("3. Password")
+                print("4. Quit")
+                edit_password_choice = input("What do you want to edit? ")
+                if edit_password_choice == "1":
+                    value_after_edit = input("What do you want the service to change to: ")
+                    accounts[edit_password_id]["Service"] = value_after_edit
+                elif edit_password_choice == "2":
+                    value_after_edit = input("What do you want the Username to change to: ")
+                    accounts[edit_password_id]["Username"] = value_after_edit
+                elif edit_password_choice == "3":
+                    value_after_edit = input("What do you want the password to change to: ")
+                    accounts[edit_password_id]["Password"] = value_after_edit
+                elif edit_password_choice == "4":
+                    return
+                else:
+                    print("That is not a valid choice!")
+        except KeyError:
+            print("That is not a valid ID!")
+            return
+
+while True:
+    print("---( Password Manager )---")
+    print("1. Add password")
+    print("2. Show passwords")
+    print("3. Delete Password")
+    print("4. Edit password")
+    print("5. Exit")
+    choice = input("\nChoose an option: ")
+
+    if choice == "1":
+        add_password()
+    elif choice == "2":
+        show_passwords()
+    elif choice == "3":
+        delete_password()
+    elif choice == "4":
+        edit_password()
+    elif choice == "5":
+        break
+    else:
+        print("That is a not a valid option!")
